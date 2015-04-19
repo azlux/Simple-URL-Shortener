70b9452dfc76d4c0d5b2134fb5d6893860db9b0f
azlux <azlux@outlook.com>
2015-04-19 16:13:34 +0300

    add icon and add the js functions file
    .

---
+++ cookie.js
@@ -0,0 +1,20 @@
+function createID() {
+    var id = (((1+Math.random())*0x10000)|0).toString(16).substring(1);
+    var today = new Date(), expires = new Date();
+    expires.setTime(today.getTime() + (360*24*60*60*1000));
+    document.cookie = "id =" + encodeURIComponent(id) + ";expires=" + expires.toGMTString();
+}
+
+function getID() {
+    var oRegex = new RegExp("(?:; )?" + "id" + "=([^;]*);?");
+
+    if (oRegex.test(document.cookie)) {
+        return decodeURIComponent(RegExp["$1"]);
+    } else {
+        createID();
+        getCookie();
+    }
+}
+
+document.getElementById("userID").setAttribute('href',"list.php?userID="+getID());
+document.getElementById("userID2").value = getID();
