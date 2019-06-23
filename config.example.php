72717c40843387dfc12a045d1e5a56d6cefebe25
azlux <github@azlux.fr>
2019-06-23 23:57:55 +0200

    rename white to light

--- config.example.php
+++ config.example.php
@@ -12,7 +12,7 @@ define('SQLITE3_FILE', './database.sqlite3');
 
 define('DEFAULT_URL', 'https://azlux.fr'); // omit the trailing slash!
 define('URL_SIZE', 5); // The lenght of your short created links
-define('WEB_THEME', 'white'); // dark or white
+define('WEB_THEME', 'dark'); // dark or light
 define('PUBLIC_INSTANCE', 'false'); // true to allow no connected people to create short url. The admin can see no connected short URL created.
 define('ALLOW_SIGNIN', 'false'); // true to allow people to signin on there own
 
